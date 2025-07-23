4fc1a8d76279219c8d61c48e57398733d4f092f6
PariAg06 <agarwalpari185@gmail.com>
2025-07-23 11:31:22 +0530

    Implement multilingual feature using Strapi CMS, added new routes for Privacy Policy and Terms & Conditions pages, and added a language change button in the header

--- build.sh
+++ build.sh
@@ -12,7 +12,9 @@ echo "✅ AWS credentials found."
 
 echo "=== Downloading AWS CLI ==="
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip -q awscliv2.zip
+
+echo "=== Installing AWS CLI locally ==="
 ./aws/install -i ./aws-cli-bin -b ./aws-cli-bin
 
 echo "=== Verifying AWS CLI version ==="
@@ -27,12 +29,12 @@ CODEARTIFACT_AUTH_TOKEN=$(./aws-cli-bin/aws codeartifact get-authorization-token
   --output text)
 
 echo "=== Logging in to CodeArtifact ==="
-npm config set //<YOUR_CODEARTIFACT_REPO>.d.codeartifact.${AWS_DEFAULT_REGION}.amazonaws.com/npm/<YOUR_REPO_NAME>/:always-auth=true
-npm config set //"<YOUR_CODEARTIFACT_REPO>.d.codeartifact.${AWS_DEFAULT_REGION}.amazonaws.com/npm/<YOUR_REPO_NAME>/:_authToken=$CODEARTIFACT_AUTH_TOKEN"
+npm config set "//<YOUR_CODEARTIFACT_REPO>.d.codeartifact.${AWS_DEFAULT_REGION}.amazonaws.com/npm/<YOUR_REPO_NAME>/:always-auth" true
+npm config set "//<YOUR_CODEARTIFACT_REPO>.d.codeartifact.${AWS_DEFAULT_REGION}.amazonaws.com/npm/<YOUR_REPO_NAME>/:_authToken" "${CODEARTIFACT_AUTH_TOKEN}"
 
 echo "✅ AWS CodeArtifact login done."
 
 echo "=== Running npm install ==="
 npm install
 
-echo "=== Build done ==="
+echo "✅ Build done ==="
